bced6b697b1b822912086db99a6ff896e469748d
Your Name <you@example.com>
2025-12-05 08:34:06 +0430

    add telemetry initialization and settings screen to navigation

--- App.tsx
+++ App.tsx
@@ -1,28 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { initTelemetry } from './src/modules/telemetry';
+import * as Sentry from '@sentry/react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { HomeScreen } from './src/screens/HomeScreen';
 import { TransferScreen } from './src/screens/TransferScreen';
 import { PerfTestScreen } from './src/screens/PerfTestScreen';
+import { SettingsScreen } from './src/screens/SettingsScreen';
 
 export type RootStackParamList = {
   Home: undefined;
   Transfers: undefined;
   PerfTest: undefined;
+  Settings: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const App: React.FC = () => {
+  useEffect(() => {
+    initTelemetry();
+  }, []);
   return (
     <NavigationContainer>
       <Stack.Navigator id="RootStack" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Transfers" component={TransferScreen} />
         <Stack.Screen name="PerfTest" component={PerfTestScreen} />
+        <Stack.Screen name="Settings" component={SettingsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
-export default App;
+export default Sentry.wrap(App);
+
