a0ef4acdf65ede1fc909d77a4be9480288c2d384
Your Name <you@example.com>
2025-12-05 07:59:23 +0430

    updated

--- App.tsx
+++ App.tsx
@@ -2,18 +2,24 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { HomeScreen } from './src/screens/HomeScreen';
+import { TransferScreen } from './src/screens/TransferScreen';
+import { PerfTestScreen } from './src/screens/PerfTestScreen';
 
-const Stack = createNativeStackNavigator();
+export type RootStackParamList = {
+  Home: undefined;
+  Transfers: undefined;
+  PerfTest: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const App: React.FC = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false
-        }}
-      >
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Transfers" component={TransferScreen} />
+        <Stack.Screen name="PerfTest" component={PerfTestScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
