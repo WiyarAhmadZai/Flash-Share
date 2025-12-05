7fb000daa32f7c2825860accd9f40807f9f29215
Your Name <you@example.com>
2025-12-05 07:59:37 +0430

    updated

--- App.tsx
+++ App.tsx
@@ -16,12 +16,10 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 const App: React.FC = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Group screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Transfers" component={TransferScreen} />
-          <Stack.Screen name="PerfTest" component={PerfTestScreen} />
-        </Stack.Group>
+      <Stack.Navigator id="RootStack" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Transfers" component={TransferScreen} />
+        <Stack.Screen name="PerfTest" component={PerfTestScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
