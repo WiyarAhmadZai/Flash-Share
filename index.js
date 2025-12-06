9437a43f2dd98a85a54d5bd4865da59b2c121ecb
Your Name <you@example.com>
2025-12-06 12:45:23 +0430

    updated

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import { registerRootComponent } from 'expo';
+import * as Sentry from '@sentry/react-native';
+import { initTelemetry } from './src/modules/telemetry';
 
 import App from './App';
+
+// Initialize Sentry before anything else.
+initTelemetry();
+
+// Wrap the app in Sentry's performance profiler.
+const WrappedApp = Sentry.wrap(App);
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(WrappedApp);
